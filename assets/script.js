54589eecaa1a54f9a512303c483cc3773e5f0a10
danbuckland1 <buckd409@gmail.com>
2018-07-06 18:16:43 -0400

    Was able to also get minutes away to print with the correct difference in time.

--- assets/script.js
+++ assets/script.js
@@ -9,73 +9,95 @@ var config = {
   firebase.initializeApp(config);
 
 
-  var database = firebase.database();
+ var database = firebase.database();
+
+var currentTime = "";
+var firstTimeConverted = "";
+var diffTime = "";
+var tRemainder = "";
+var tMinutesTillTrain = "";
+var nextTrain = "";
+
 
 
   $("#add-train").on("click", function(event){
-      event.preventDefault();
+    event.preventDefault();
 
-     var trainName = $("#train-name").val().trim();
-     var trainDestination = $("#train-destination").val().trim();
-     var firstTrainTime = $("#first-train").val().trim();
-     var trainFrequency = $("#train-frequency").val().trim();
-
-      var newTrain = {
-        train: trainName,
-        destination: trainDestination,
-        firstTime: firstTrainTime,
-        frequency: trainFrequency
-
-      };
-
-      database.ref().push(newTrain);
-
-      console.log(newTrain.train);
-      console.log(newTrain.destination);
-      console.log(newTrain.firstTime);
-      console.log(newTrain.frequency);
-
-
-    $("#train-name").val("");
-    $("#train-destination").val("");
-    $("#first-train").val("");
-    $("#train-frequency").val("");
-
-  });
-
-  database.ref().on("child_added", function(childSnapShot){
-    console.log(childSnapShot.val());
-
-    var newTrain = childSnapShot.val().train;
-    var newDestination = childSnapShot.val().destination;
-    var newFirstTrain = childSnapShot.val().firstTime;
-    var newFrequency = childSnapShot.val().frequency;
-
-    console.log(newTrain);
-    console.log(newDestination);
-    console.log(newFirstTrain);
-    console.log(newFrequency);
-
-    var newRow = $("<tr>").append(
-      $("<td>").text(newTrain),
-      $("<td>").text(newDestination),
-      $("<td>").text(newFrequency),
-      $("<td>").text(newFirstTrain),
-  
-    );
-
-    $("#train-table > tbody").append(newRow);
-
-  });
-
+   var trainName = $("#train-name").val().trim();
+   var trainDestination = $("#train-destination").val().trim();
+   var firstTrainTime = $("#first-train").val().trim();
+   var trainFrequency = $("#train-frequency").val().trim();
+   var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+   currentTime = moment();
+   diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+   tRemainder = diffTime % trainFrequency;
+   tMinutesTillTrain = trainFrequency - tRemainder;
+   nextTrain = moment().add(tMinutesTillTrain, "minutes");
+   
   
 
+    var newTrain = {
+      train: trainName,
+      destination: trainDestination,
+      firstTime: firstTrainTime,
+      frequency: trainFrequency,
+      tMinutesTillTrain: tMinutesTillTrain,
+
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.train);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTime);
+    console.log(newTrain.frequency);
+    console.log(newTrain.tMinutesTillTrain);
+    
+
+
+  $("#train-name").val("");
+  $("#train-destination").val("");
+  $("#first-train").val("");
+  $("#train-frequency").val("");
+
+  return false;
+
+});
+
+database.ref().on("child_added", function(childSnapShot){
+  console.log(childSnapShot.val());
+
+  var newTrain = childSnapShot.val().train;
+  var newDestination = childSnapShot.val().destination;
+  var newFirstTrain = childSnapShot.val().firstTime;
+  var newFrequency = childSnapShot.val().frequency;
+  var newMinutesAway = childSnapShot.val().tMinutesTillTrain
+
+  
   
 
+  console.log(newTrain);
+  console.log(newDestination);
+  console.log(newFirstTrain);
+  console.log(newFrequency);
+  console.log(newMinutesAway);
 
+  
 
+  var newRow = $("<tr>").append(
+    $("<td>").text(newTrain),
+    $("<td>").text(newDestination),
+    $("<td>").text(newFrequency),
+    $("<td>").text(newFirstTrain),
+    $("<td>").text(newMinutesAway),
 
-   // var currentDate = moment();
-  // console.log(currentDate.format("MM/DD/YYYY hh:mm a"));
+  );
+
+  $("#train-table > tbody").append(newRow);
+
+});
+
+   var currentDate = moment();
+  console.log(currentDate.format("MM/DD/YYYY hh:mm a"));
 
     
